1a3b38308362ed2d4ec166fc0907a7a190d0fd7a
Francesco <francesco.savignano96@gmail.com>
2020-11-12 14:56:56 +0100

    modificata struttura dati, aggiunto più contatti, e stampati i nomi per testare il v-for sulla lista

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var app = new Vue(
             contacts: [
                 {
                     name: 'Michele',
-                    avatar: '_1',
+                    avatar: '_2',
                     visible: true,
                     messages:   [
                         {
@@ -27,7 +27,7 @@ var app = new Vue(
                 },
                 {
                     name: 'Fabio',
-                    avatar: '_2',
+                    avatar: '_3',
                     visible: true,
                     messages: [
                         {
@@ -41,16 +41,15 @@ var app = new Vue(
                             status: 'received'
                         },
                         {
-                        date: '20/03/2020 16:35:00',
-                            message: 'Mi piacerebbe ma devo andare a fare la
-                            spesa',
+                            date: '20/03/2020 16:35:00',
+                            message: 'Mi piacerebbe ma devo andare a fare la spesa',
                             status: 'received'
                         }
                     ],
                 },
                 {
                     name: 'Samuele',
-                    avatar: '_3',
+                    avatar: '_4',
                     visible: true,
                     messages: [
                         {
@@ -72,7 +71,7 @@ var app = new Vue(
                 },
                 {
                     name: 'Luisa',
-                    avatar: '_4',
+                    avatar: '_5',
                     visible: true,
                     messages: [
                         {
@@ -87,13 +86,80 @@ var app = new Vue(
                         }
                     ],
                 },
+                {
+                    name: 'Giacomo',
+                    avatar: '_6',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/10/2020 15:00:55',
+                            message: 'Ti ricordi di me?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/10/2020 15:50:00',
+                            message: 'Certo che si!',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Mirco',
+                    avatar: '_7',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '11/02/2020 13:25:55',
+                            message: 'Come va?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '11/02/2020 15:00:00',
+                            message: 'Bene tu?',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Anna',
+                    avatar: '_8',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '14/03/2020 12:30:55',
+                            message: 'Che film mi consigli?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '14/03/2020 12:50:00',
+                            message: 'Il signore degli anelli',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Beatrice',
+                    avatar: '_9',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '15/03/2020 15:30:55',
+                            message: 'Ciao',
+                            status: 'sent'
+                        },
+                        {
+                            date: '15/03/2020 15:50:00',
+                            message: 'Ciao',
+                            status: 'received'
+                        }
+                    ],
+                },
             ]
         },
         methods: {
-            
-        },
-        mounted() {
 
         }
     }
 );
+
+console.log(app.contacts);
